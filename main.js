291c585cf80840a6ad7ef0ffa341cfa53a88ddda
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-06-06 18:39:00 +0200

    change js codes to obj

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-let d1=document.querySelector('.dice-1');
-let d2=document.querySelector('.dice-2');
-let botton=document.querySelector('.botton');
 
-botton.addEventListener('click',()=>{
-let random1 = Math.floor((Math.random() * 6) + 1);
-let random2 = Math.floor((Math.random() * 6) + 1);
-d1.classList.toggle(`dice-${random1}`);
-d2.classList.toggle(`dice-${random2}`);
+let diceOne={
+val:6,
+roll:()=>{
+let randomN = Math.floor((Math.random() * diceOne.val) + 1) 
+document.querySelector('.con div:first-child').className =`dice-${randomN}`  
+}
+
+}
+
+let diceTow={
+    val:6,
+    roll:()=>{
+    let randomN = Math.floor((Math.random() * diceTow.val) + 1) 
+    document.querySelector('.con div:last-child').className =`dice-${randomN}`   
+      
+}
+
+}
+
+document.querySelector('.botton').addEventListener('click',()=>{
+    diceOne.roll()
+    diceTow.roll()
 })
